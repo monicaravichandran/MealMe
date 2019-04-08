dbc70835e40cb42809c1157816c41ac2c1ea8bd9
Kalyan Vejalla <kalyan.vejalla392@gmail.com>
2019-04-07 21:28:57 -0700

    setting up firebase

---
+++ MealMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MealMe
+//
+//  Created by Kalyan Vejalla on 4/7/19.
+//  Copyright © 2019 Kalyan Vejalla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
